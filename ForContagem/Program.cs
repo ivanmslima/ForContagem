a10b60740f8868f5c388037f89c805405fe8c60b
ivanmslima <81934424+ivanmslima@users.noreply.github.com>
2022-11-14 22:17:38 -0300

    Adicionar arquivos de projeto.

---
+++ ForContagem/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Globalization;
+
+namespace curso
+{
+    class Progam
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine("Valor de i:" + i);
+            }
+            Console.WriteLine();
+
+
+        }
+    }
+}
